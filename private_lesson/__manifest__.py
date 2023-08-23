a2561f088e192deb553e1e4ad74e20874a33beff
Khaled Emad <khaledemad656@gmail.com>
2023-08-23 15:01:46 +0300

    start odoo module

---
+++ private_lesson/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "private_lesson",
+    "summary": """Plusone Task""",
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "private_lesson",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": ["base"],
+    "installable": True,
+    "application": True,
+    # always loaded
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/views.xml",
+        "views/templates.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [
+        "demo/demo.xml",
+    ],
+}
