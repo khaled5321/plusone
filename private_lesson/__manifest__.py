935258131fdb6db5ad70c6202b44e73e4f687886
Khaled Emad <khaledemad656@gmail.com>
2023-08-23 17:16:07 +0300

    edit

--- private_lesson/__manifest__.py
+++ private_lesson/__manifest__.py
@@ -15,9 +15,11 @@
     "application": True,
     # always loaded
     "data": [
-        # 'security/ir.model.access.csv',
+        "security/ir.model.access.csv",
         "views/views.xml",
         "views/templates.xml",
+        "views/menu.xml",
+        # "data/subjects.csv",
     ],
     # only loaded in demonstration mode
     "demo": [
--- private_lesson/models/models.py
+++ private_lesson/models/models.py
@@ -1,18 +1,38 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class private_lesson(models.Model):
-#     _name = 'private_lesson.private_lesson'
-#     _description = 'private_lesson.private_lesson'
+def _get_lesson_subjects(self):
+    return [("physics", "Physics"), ("chemistry", "Chemistry"), ("math", "Math")]
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+class private_lesson(models.Model):
+    _name = "private_lesson"
+    _description = "Private lesson"
+
+    subject = fields.Selection(
+        _get_lesson_subjects,
+        string="Subject",
+        required=True,
+        readonly=True,
+    )
+
+    lesson_name = fields.Char(string="Lesson Name", compute="_compute_lesson_name")
+
+    student_id = fields.Many2one("res.partner", string="Student", required=True)
+    teacher_id = fields.Many2one("res.partner", string="Teacher", required=True)
+
+    @api.depends("subject", "create_date")
+    def _compute_lesson_name(self):
+        for lesson in self:
+            lesson.lesson_name = (
+                f"{lesson.subject}-{lesson.create_date.year}-{lesson.create_date.month}"
+            )
+
+    @api.onchange("student_id")
+    def _onchange_student_id(self):
+        self.teacher_id = False
+        if self.student_id:
+            country_id = self.student_id.country_id
+            self.teacher_id = self.env["res.partner"].search(
+                [("country_id", "=", country_id)]
+            )
